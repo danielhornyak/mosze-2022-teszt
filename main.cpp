1c597a72fbc137db976bb4114a69e1211da7fc09
Hornyák Dániel <hornyada@gmail.com>
2022-09-08 22:26:10 +0200

    MOSZE git code javitva commit

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // Nem létezik ilyen nevű statikus változó.
-    std::cout << '1-100 ertekek duplazasa' // Nincs pontos vesző a sor végén és hiányzik << std::endl.
-    for (int i = 0;) //rossz szintaxisú for ciklus helyes for(int i=0; i < N_ELEMENTS; i++)
+    int *b = new int[N_ELEMENTS]; // Nem létezik ilyen nevű statikus változó.
+    std::cout << "1-100 ertekek duplazasa"<< std::endl; // Nincs pontos vesző a sor végén és hiányzik << std::endl.
+    for(int i=0; i < N_ELEMENTS; i++) //rossz szintaxisú for ciklus helyes for(int i=0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2; // helyesen b[i] = (i + 1) * 2;
+        b[i] = (i + 1) * 2; // helyesen b[i] = (i + 1) * 2;
     }
-    for (int i = 0; i; i++) // rossz szintaxisú for ciklus helyes for (int i = 0; i < N_ELEMENTS ; i++)
+    std::cout << "Ertek:";
+    for (int i = 0; i < N_ELEMENTS ; i++) // rossz szintaxisú for ciklus helyes for (int i = 0; i < N_ELEMENTS ; i++)
     {
-        std::cout << "Ertek:" // Nincs pontos vesző a sor végén és az igy mindig kiírja az Ertek szót ezt a for cikluson elé kell rakni és még nem írja ki a b tömb értékeit sem.
+        std::cout << b[i]<<' '; // Nincs pontos vesző a sor végén és az igy mindig kiírja az Ertek szót ezt a for cikluson elé kell rakni és még nem írja ki a b tömb értékeit sem.
     }    
-    // itt édemes új sort kezdeni std::cout << std::endl;
+    std::cout << std::endl;// itt édemes új sort kezdeni std::cout << std::endl;
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // Ha egészre kerekített átlagra van szükség, akkor jó egyébként az átlagnak double típusunak kell lennie.
-    for (int i = 0; i < N_ELEMENTS, i++) //i++ elött lévő vesző helyet pontos vesszőnek kell lenie.
+    double atlag; // Ha egészre kerekített átlagra van szükség, akkor jó egyébként az átlagnak double típusunak kell lennie.
+    for (int i = 0; i < N_ELEMENTS; i++) //i++ elött lévő vesző helyet pontos vesszőnek kell lenie.
     {
-        atlag += b[i]
+        atlag += b[i]; // hiányzik a pontos vessző;
     }
-    // Itt még kell egy delete[] b;
+    delete[] b;// Itt még kell egy delete[] b;
     atlag /= N_ELEMENTS; 
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
